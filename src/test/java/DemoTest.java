bdda4c89823a5dc486b36ba7fd6fa202beed7cc7
DinuraHashini <113836830+DinuraHashini@users.noreply.github.com>
2022-09-18 19:50:25 +0530

    add test case

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,81 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
+/**
+ * The class containing your tests for the {@link Demo} class. Make sure you
+ * test all methods in this class (including both {@link Demo#main(String[])}
+ * and {@link Demo#isTriangle(double, double, double)}).
+ */
+public class DemoTest {
+
+	@Test
+	public void test_is_triangle_1() {
+		assertTrue(Demo.isTriangle(3, 4, 5));
+	}
+
+	@Test 
+	public void test_is_triangle_2() {
+		assertTrue(Demo.isTriangle(5,12,13));
+	}
+	
+	@Test
+	public void test_is_triangle_3() {
+		assertTrue(Demo.isTriangle(5, 13, 12));	
+	}
+	
+	@Test
+	public void test_is_triangle_4() {
+		assertTrue(Demo.isTriangle(12, 5, 13));
+	}
+	
+	@Test
+	public void test_is_triangle_5() {
+		assertTrue(Demo.isTriangle(6, 8, 10));
+	}
+	
+	@Test
+	public void test_is_triangle_6() {
+		assertTrue(Demo.isTriangle(10, 8, 6));
+	}
+		
+	@Test
+	public void test_is_not_triangle_1() {
+		assertFalse(Demo.isTriangle(5, 7, 13));
+		
+	}
+	
+	@Test
+	public void test_is_not_triangle_2() {
+		assertFalse(Demo.isTriangle(5, 13, 7));
+	}
+	
+	@Test
+	public void test_is_not_triangle_3() {
+		assertFalse(Demo.isTriangle(13, 5, 7));
+	}
+		
+	@Test
+	public void test_is_not_triangle_4() {
+		assertFalse(Demo.isTriangle(13, 7, 5));
+	}	
+		
+	@Test
+	public void test_is_not_triangle_5() {
+		assertFalse(Demo.isTriangle(5, 9, 3));
+	}
+	
+	@Test
+	public void test_is_not_triangle_6() {
+		assertFalse(Demo.isTriangle(1, 2, -1));
+
+	}	
+	
+	@Test
+	public void test_is_not_triangle_7() {
+		assertFalse(Demo.isTriangle(0, 3, 5));
+	}
+}
